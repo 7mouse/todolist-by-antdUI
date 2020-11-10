fda175f21bb87de7fe7e1721e260c85a6faa1ce9
LoneMouse <406435866@qq.com>
2020-11-10 21:47:25 +0800

    简单的自适应 适配手机

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Search from '@components/Search/Search.js'
+import './style.css'
 
 function HeaderTop(props) {
   return (
-    <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-evenly'}}>
+    <div className="header-panel">
       <h1 style={{marginTop: '10px'}}>待办事项: {props.count}</h1>
       <Search />
     </div>
